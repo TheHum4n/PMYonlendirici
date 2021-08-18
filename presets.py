5c394a71703b85fd13546c58179c53bfb9565220
@TheHuman <76818629+TheHum4n@users.noreply.github.com>
2021-08-19 02:19:09 +0300

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,6 +1,6 @@
 #-------------------------------------- https://github.com/m4mallu/PMChatbot ------------------------------------------#
 class Presets(object):
-    WELCOME_TEXT = "🇹🇷Merhaba... \n<code>Mesajınızı buraya yazın...\nMesajınızı en kısa sürede yanıtlayacağım</code> 🙂\n🇦🇿Salam... \n<code>Mesajınızı bura yazın...\nMesajınıza ən erkən cavab verəcəm</code> 🙂\n🇬🇧Hello... \n<code>Type your query here..\nI'll respond to your query as earliest</code> 🙂"
+    WELCOME_TEXT = "🇹🇷Merhaba... \n<code>Mesajınızı buraya yazın...\nMesajınızı en kısa sürede yanıtlayacağım.</code> 🙂\n🇦🇿Salam... \n<code>Mesajınızı bura yazın...\nMesajınıza ən erkən cavab verəcəm.</code> 🙂\n🇬🇧Hello... \n<code>Type your message here..\nI'll respond to your message as earliest.</code> 🙂"
     USER_DETAILS = "<b>PM FROM:</b>\nName: {} {}\nId: {}\nUname: @{}\nScam: {}\nRestricted: {}\nStatus: {}\nDc Id: {}"
     PM_TXT_ATT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
     PM_MED_ATT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
